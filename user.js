b7d511e6b097bbf82413985e9f525232399d3697
eddybbrownii <eddybbrownii@gmail.com>
2026-04-19 13:23:53 -1000

    19042026

---
+++ user.js
@@ -0,0 +1,11 @@
+localStorage.getItem ('id');
+
+async function main () {
+  const id = localStorage.getItem ('id');
+  const posts = await fetch (
+    'https://jsonplaceholder.typicode.com/posts?userId=:id"'
+  );
+  const postsData = await posts.json ();
+}
+
+main ();
