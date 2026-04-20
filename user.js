3a8813d86994f38036a56754a741da8987b2b088
eddybbrownii <eddybbrownii@gmail.com>
2026-04-19 14:32:39 -1000

    19042026

--- user.js
+++ user.js
@@ -1,26 +1,22 @@
-const postListEl = document.querySelector ('.post-list');
-const id = localStorage.getItem ('id');
-async function onSearchChange (event) {
-  const id = event.target.value;
-  renderPosts (id);
-}
-async function renderPosts (id) {
+async function main () {
+  const id = localStorage.getItem ('id');
   const posts = await fetch (
     `https://jsonplaceholder.typicode.com/posts?userId=${id}`
   );
   const postsData = await posts.json ();
+  const postListEl = document.querySelector ('.post-list');
   postListEl.innerHTML = postsData.map (post => postHTML (post)).join ('');
+
+  postsData.map (
+    post => `
+    <div class="post">
+    <div class="post__title">
+    <h3>${post.title}</h3>
+    </div>
+    <div class="post__body">
+    <p>${post.body}</p>
+    </</div>`
+  );
 }
 
-function postHTML (post) {
-  return;
-  `<div class="post">
-    <div class="pst__title">
-    ${post.title}
-    </div>
-    <p class="post__body">
-    ${post.body}
-    </p>
-    </div>`.join ('');
-}
-renderPosts (id);
+main ();
